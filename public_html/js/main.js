4e8cc446a46bd2c44ba229fa3eb79ddd3a5f1122
Eren Kumru <eren_kumru@hotmail.com>
2021-12-28 23:57:07 +0300

    Error removal and add Decision Tree to main for test purposes

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -55,7 +55,7 @@ function main(){
         raycaster.setFromCamera( clickMouse, camera );
         
         // calculate objects intersecting the picking ray
-	const found = intersect(clickMouse);;
+	const found = intersect(clickMouse);
         if(found.length > 0 && found[0].object.userData.draggable){
             draggable = found[0].object;
             console.log("found draggable " + draggable.userData.name);
@@ -442,6 +442,72 @@ function main(){
     //-----------------NEW SHADER TEST ENDS---------------------------------------------------------------------------//
 
 
+    //-----------------DECISION TREE TEST STARTS----------------------------------------------------------------------//
+//Import DecisionTree
+    var DecisionTree = require('decision-tree');
+
+//Create training data
+//Test data is compared to this data to check accuracy
+//Predictions are made on this data to give results
+    var training_data = [
+        {"color":"blue", "shape":"square", "liked":false},
+        {"color":"red", "shape":"square", "liked":false},
+        {"color":"blue", "shape":"circle", "liked":true},
+        {"color":"red", "shape":"circle", "liked":true},
+        {"color":"blue", "shape":"hexagon", "liked":false},
+        {"color":"red", "shape":"hexagon", "liked":false},
+        {"color":"yellow", "shape":"hexagon", "liked":true},
+        {"color":"yellow", "shape":"circle", "liked":true}
+    ];
+
+//Used to check the accuracy of the model
+    var test_data = [
+        {"color":"blue", "shape":"hexagon", "liked":false},
+        {"color":"red", "shape":"hexagon", "liked":false},
+        {"color":"yellow", "shape":"hexagon", "liked":true},
+        {"color":"yellow", "shape":"circle", "liked":true}
+    ];
+
+    var class_name = "liked";
+
+
+    var features = ["color", "shape"];
+
+    var dt = new DecisionTree(class_name, features);
+
+
+    dt.train(training_data);
+
+
+    var predicted_class = dt.predict({
+        color: "blue",
+        shape: "hexagon"
+    });
+
+
+    var accuracy = dt.evaluate(test_data);
+
+
+    console.log("dt:", dt);
+    console.log("predicted_class:", predicted_class);
+    console.log("Accuracy:", accuracy);
+
+    /* Import/Export trained model
+    //To export (save) trained model for future use
+    var treeJson = dt.toJSON();
+    console.log("treeJson: ", treeJson);
+    //Create a new model
+    var treeJson = dt.toJSON();
+    var preTrainedDecisionTree = new DecisionTree(treeJson);
+    //Alternately, you can also import a previously trained model on an existing tree instance,
+    assuming the features & class are the same:
+    var treeJson = dt.toJSON();
+    dt.import(treeJson);
+    */
+
+
+    //-----------------DECISION TREE TEST ENDS------------------------------------------------------------------------//
+
     // add pine tree to scene
     createPine(new THREE.Vector3( 0, 0, 0 ));
     
@@ -530,7 +596,7 @@ function keyEvents(){
        
         window.addEventListener("keydown", function(event){
             
-        if(event.key ==	"ArrowRight"){
+        if(event.key === "ArrowRight"){
             //spotLight.target.translateX(1.0);
             spotLight.position.x += 1;
             }
@@ -587,7 +653,7 @@ function intersect(pos) {
 function dragObject(){
     if(draggable != null){
        
-        const found = intersect(mouseMove);;
+        const found = intersect(mouseMove);
         if(found.length > 0){
             for( let o of found){
                 if(o.object.userData.ground){
